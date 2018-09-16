0f3d58a8591ec93f636b5dbdb5e52bed3cca92fd
jordyhuana <jordyhuana@hotmail.com>
2018-09-16 11:56:46 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,3 +13,36 @@ client.on('message', message => {
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
+
+{
+  "ClientId": 319928722901958656,
+  "Token": "MzE5OTI4NzIyOTAxOTU4NjU2.Dn3a0g.GNnrR5LpfzrFD8G5w9XgdWYrT_c",
+  "OwnerIds": [
+  151447500232851456,
+  ],
+  "LoLApiKey": "",
+  "GoogleApiKey": "AIzaSyDQ1sSErvX20BfnjRH5pSGpe6YQjj3twV4",
+  "MashapeKey": "",
+  "OsuApiKey": "",
+  "SoundCloudClientId": "",
+  "CleverbotApiKey": "",
+  "CarbonKey": "",
+  "Db": {
+    "Type": "sqlite",
+    "ConnectionString": "Data Source=data/NadekoBot.db"
+  },
+  "TotalShards": 1,
+  "PatreonAccessToken": "",
+  "PatreonCampaignId": "334038",
+  "RestartCommand": null,
+  "ShardRunCommand": "",
+  "ShardRunArguments": "",
+  "ShardRunPort": null,
+  "MiningProxyUrl": null,
+  "MiningProxyCreds": null,
+  "BotListToken": null,
+  "TwitchClientId": null,
+  "VotesToken": null,
+  "VotesUrl": null,
+  "RedisOptions": null
+}
